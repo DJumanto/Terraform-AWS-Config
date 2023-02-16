822eecc8899a03877923c30d6fe7415672d17597
Alfa Fakhrur <100863813+DJumanto@users.noreply.github.com>
2023-02-16 11:58:50 +0700

    Enable Access

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+variable "AWS_SECRET_ACCESS_KEY" {
+    type = string
+    default = null
+}
+variable "AWS_ACCESS_KEY_ID" {
+    type = string
+    default = null
+}
 variable "aws_region" {
     description = "Region for AWS EC2 instances"
     default = "ap-southeast-1"
